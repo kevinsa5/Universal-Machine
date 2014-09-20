188b1f760c2a35497c8472ebb69b7fc6f86ac374
Kevin anderson <kevinanderso@gmail.com>
2014-09-19 20:54:00 -0400

    switched to hashtable from linked list

--- uvm.c
+++ uvm.c
@@ -4,13 +4,15 @@
 #include <sys/stat.h>
 #include <inttypes.h>
 
+#include "uthash.h"
+
 off_t fsize(const char*);
 
 struct node {
 	int id;
 	uint32_t* array;
 	int len;
-	struct node *next;
+	UT_hash_handle hh;
 };
 
 int main(int argc, char* argv[]){
@@ -19,7 +21,8 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 	uint32_t reg[] = {0,0,0,0,0,0,0,0};
-	struct node *collection = (struct node*)malloc(sizeof(struct node));
+	struct node *collection = NULL;
+	struct node *head = (struct node*)malloc(sizeof(struct node));
 	uint32_t pc = 0;
 	uint32_t serial = 1;
 	FILE *progfile;
@@ -28,36 +31,36 @@ int main(int argc, char* argv[]){
 		printf("Unable to open %s", argv[1]);
 		return 1;
 	}
-	collection->id = 0;
-	collection->len = fsize(argv[1])/sizeof(uint32_t);
-	collection->next = 0;
+	head->id = 0;
+	head->len = fsize(argv[1])/sizeof(uint32_t);
 	uint32_t *buffer;
-	buffer = malloc((collection->len) * sizeof(uint32_t));
-	fread(buffer, 4, (collection->len), progfile);	
+	buffer = malloc((head->len) * sizeof(uint32_t));
+	fread(buffer, 4, (head->len), progfile);	
 	//swap endianness? not kosher to assume, but...
 	int i;
-	for(i = 0; i < (collection->len); i++){
+	for(i = 0; i < (head->len); i++){
 		uint32_t temp = buffer[i];
 		buffer[i] = ((temp & 0xFF000000) >> 24) | 
 					((temp & 0x00FF0000) >> 8)  |
 					((temp & 0x0000FF00) << 8)  |
 					((temp & 0x000000FF) << 24);
 	}
-	collection->array = buffer;
+	head->array = buffer;
+	HASH_ADD_INT(collection, id, head);
 	puts("executing");
 	/*
-	for(i = 0; i < (collection->len); i++){
-		printf("%d:%08x\n",i,(collection->array)[i]);
+	for(i = 0; i < (head->len); i++){
+		printf("%d:%08x\n",i,(head->array)[i]);
 
 	}
 	*/
-	struct node* iter;
+	struct node *n;
 	while(1){
-		if(pc > collection->len){
-			printf("out of bounds: tried to access %d; max %d\n",pc,collection->len);
+		if(pc > head->len){
+			printf("out of bounds: tried to access %d; max %d\n",pc,head->len);
 			return 2;
 		}
-		uint32_t platter = (collection->array)[pc];
+		uint32_t platter = (head->array)[pc];
 		pc++;
 		char opcode = (platter >> 28);
 		char A = (platter >> 6) & 0b111;
@@ -68,11 +71,6 @@ int main(int argc, char* argv[]){
 			int i;
 			for(i = 0; i < 8; i++) printf("%" PRIu32 " ", reg[i]);
 			puts("");
-			iter = collection;
-			while(iter != 0){
-				printf("%d ", iter->id);
-				iter = iter->next;
-			}
 			getchar();
 		}
 		switch(opcode){
@@ -80,41 +78,28 @@ int main(int argc, char* argv[]){
 			if(reg[C] != 0)
 				reg[A] = reg[B];
 			break;
-		case 1:
-			iter = collection;
-			while(iter != 0){
-				if (iter->id == reg[B]){
-					if(reg[C] > (iter->len)){
-						printf("opcode 1, out of bounds\n");
-						return 2;
-					}
-					reg[A] = (iter->array)[reg[C]];
-					break;
-				}
-				iter = iter->next;
-			}
-			if(iter == 0){
+		case 1: 
+			n = NULL;
+			HASH_FIND_INT(collection, &(reg[B]), n);
+			if(n == NULL){
 				printf("opcode 1, no such array found:%d\n",reg[B]);
 				return 2;
 			}
+			reg[A] = (n->array)[reg[C]];
 			break;
-		case 2:
-			iter = collection;
-			while(iter != 0){
-				if (iter->id == reg[A]){
-					if(reg[B] > (iter->len)){
-						printf("opcode 2, out of bounds: accessing %d with max %d\n", reg[B], iter->len);
-						return 2;
-					}
-					(iter->array)[reg[B]] = reg[C];
-					break;
-				}
-				iter = iter->next;
-			}
-			if(iter == 0){
+		case 2: 
+			n = NULL;
+			HASH_FIND_INT(collection, &(reg[A]), n);
+			if(n == NULL){
 				puts("opcode 2, no such array found");
 				return 2;
 			}
+			if(reg[B] > (n->len)){
+				printf("opcode 2, out of bounds: accessing %d with max %d\n", reg[B], n->len);
+				return 2;
+			}
+			(n->array)[reg[B]] = reg[C];
+			
 			break;
 		case 3:
 			reg[A] = reg[B] + reg[C];
@@ -136,21 +121,16 @@ int main(int argc, char* argv[]){
 			puts("halting");
 			return 0;
 		case 8:
-			// labels must be followed by statements, not declarations
-			;
-			struct node *new = (struct node*)malloc(sizeof(struct node));
-			new->id = serial;
-			new->len = reg[C];
-			new->next = 0;
+			n = (struct node*)malloc(sizeof(struct node));
+			n->id = serial;
+			n->len = reg[C];
+
 			uint32_t *arr;
-			arr = malloc((new->len) * sizeof(uint32_t));
-			int i;
-			for(i = 0; i < new->len; i++) arr[i] = (uint32_t) 0;
-			new->array = arr;
-			struct node *second = collection->next;
-			collection->next = new;
-			new->next = second;
-			reg[B] = new->id;
+			arr = malloc((n->len) * sizeof(uint32_t));
+			for(i = 0; i < reg[C]; i++) arr[i] = (uint32_t) 0;
+			n->array = arr;
+			HASH_ADD_INT(collection, id, n);
+			reg[B] = n->id;
 			serial++;
 			break;
 		case 9:
@@ -158,22 +138,15 @@ int main(int argc, char* argv[]){
 				puts("can't free the 0 array");
 				return 2;
 			}
-			iter = collection;
-			struct node* prev;
-			while(iter != 0){
-				if (iter->id == reg[C]){
-					prev->next = iter->next;
-					free(iter->array);
-					free(iter);
-					break;
-				}
-				prev = iter;
-				iter = iter->next;
-			}
-			if(iter == 0){
-				printf("opcode 9, no such array found: %d\n", reg[C]);
+			struct node *n = NULL;
+			HASH_FIND_INT(collection, &(reg[C]), n);
+			if(n == NULL){
+				puts("opcode 9, no such array found");
 				return 2;
 			}
+			HASH_DEL(collection, n);
+			free(n->array);
+			free(n);
 			break;
 		case 10:
 			printf("%c", reg[C]);
@@ -182,24 +155,18 @@ int main(int argc, char* argv[]){
 			printf("input:");
 			reg[C] = getchar();
 			break;
-		case 12:
-			iter = collection;
-			while(iter != 0){
-				if (iter->id == reg[B]){
-					collection->array = (uint32_t*) realloc(collection->array,(iter->len) * sizeof(uint32_t));
-					int i;
-					for(i = 0; i < iter->len; i++)
-						(collection->array)[i] = (iter->array)[i];
-					collection->len = iter->len;
-					pc = reg[C];
-					break;
-				}
-				iter = iter->next;
-			}
-			if(iter == 0){
-				puts("opcode 12 no such array");
+		case 12: 
+			n = NULL;
+			HASH_FIND_INT(collection, &(reg[B]), n);
+			if(n == NULL){
+				puts("opcode 12, no such array found");
 				return 2;
 			}
+			head->array = (uint32_t*) realloc(head->array,(n->len) * sizeof(uint32_t));
+			for(i = 0; i < n->len; i++)
+				(head->array)[i] = (n->array)[i];
+			head->len = n->len;
+			pc = reg[C];
 			break;
 		case 13:
 			A = (platter >> 25) & 0b111;
